b57eaedecc2e5b6be77f4f3025089f4daf399c41
facedo20 <110402210+facedo20@users.noreply.github.com>
2023-03-18 22:18:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ async def ruta():
     return {'PRIMER API DE FEDERICO ACEDO'}
 
 #cargamos los csv para empezar a trabajar 
-listado_gral = pd.read_csv('PI1FA\ETL.csv')
+listado_gral = pd.read_csv('.\ETL.csv')
 
 @app.get('/max')
 #funcion 1 : devulve la pelicula con mayor duracion del año indicado segun cada plataforma
